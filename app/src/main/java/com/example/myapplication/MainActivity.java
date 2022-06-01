cb20002af632405cffa198c055a9cc9cd849f64e
plasmacluster <51335231+plasmacluster@users.noreply.github.com>
2022-06-01 11:05:58 +0900

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,158 @@
+package com.example.myapplication;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Environment;
+import android.os.FileUtils;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.Toast;
+
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private com.example.myapplication.DrawingView mDrawingView;
+    private static int COLOR_PANEL = 0;
+    private static int BRUSH = 0;
+    private ImageButton mColorPanel;
+    private ImageButton mBrush;
+    private ImageButton mUndo;
+    private ImageButton mSave;
+    private static final int MY_PERMISSIONS_REQUEST_READ_CONTACTS = 1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initViews();
+        initPaintMode();
+        loadImage();
+        checkPermissions();
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    private void initViews() {
+        mDrawingView = (com.example.myapplication.DrawingView) findViewById(R.id.img_screenshot);
+        mBrush = (ImageButton) findViewById(R.id.brush);
+        mColorPanel = (ImageButton) findViewById(R.id.color_panel);
+        mUndo = (ImageButton) findViewById(R.id.undo);
+        mSave = (ImageButton) findViewById(R.id.save);
+
+        mBrush.setOnClickListener(this);
+        mColorPanel.setOnClickListener(this);
+        mUndo.setOnClickListener(this);
+        mSave.setOnClickListener(this);
+        initPaintMode();
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    private void initPaintMode() {
+        mDrawingView.initializePen();
+        mDrawingView.setPenSize(10);
+        mDrawingView.setPenColor(getColor(R.color.red));
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.brush:
+                mBrush.setImageResource(BRUSH == 0 ? R.drawable.ic_brush : R.drawable.ic_pen);
+                mDrawingView.setPenSize(BRUSH == 0 ? 40 : 10);
+                BRUSH = 1 - BRUSH;
+                break;
+            case R.id.color_panel:
+                mColorPanel.setImageResource(COLOR_PANEL == 0 ? R.drawable.ic_color_blue : R.drawable.ic_color_red);
+                mDrawingView.setPenColor(COLOR_PANEL == 0 ? getColor(R.color.blue) : getColor(R.color.red));
+                COLOR_PANEL = 1 - COLOR_PANEL;
+                break;
+            case R.id.undo:
+                mDrawingView.undo();
+                break;
+            case R.id.save:
+                saveImage();
+                break;
+            default:
+                break;
+        }
+    }
+
+    public void loadImage() {
+        Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.raw.image);
+        mDrawingView.loadImage(bitmap);
+    }
+
+    public void saveImage() {
+        if (!checkPermissions()) {
+            return;
+        }
+        String sdPicturesPath = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES).toString();
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss", Locale.CHINA).format(new Date());
+        String filename = sdPicturesPath + "/DrawImage_" + timeStamp;
+      /*  if (FileUtils.saveBitmap(filename, mDrawingView.getImageBitmap(), Bitmap.CompressFormat.PNG, 100)) {
+            Toast.makeText(this, "Save Success", Toast.LENGTH_SHORT).show();
+        }*/
+    }
+
+    public boolean checkPermissions() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+            // Permission is not granted
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+                // Show an explanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+            } else {
+                // No explanation needed; request the permission
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                        MY_PERMISSIONS_REQUEST_READ_CONTACTS);
+
+                // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
+                // app-defined int constant. The callback method gets the
+                // result of the request.
+            }
+            return false;
+        } else {
+            // Permission has already been granted
+            return true;
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                                           int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_READ_CONTACTS: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // permission was granted, yay! Do the
+                    // contacts-related task you need to do.
+                } else {
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                }
+                return;
+            }
+
+            // other 'case' lines to check for other
+            // permissions this app might request.
+        }
+    }
+
+}
